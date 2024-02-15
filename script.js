fcfb2f2b3f14ecb1610f1d69d7f839de496c0d56
Dhruv Patel <122099280+dhruvurgr8@users.noreply.github.com>
2024-02-15 18:25:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function firstWord(s) {
-  // your code here
+	let str = "";
+  if(s==""){
+	  return s;
+  }else{
+	  for(let i=0;i<s.length;i++){
+		  if(s[i]==" "){
+			  return str;
+		  }else{
+			  str+=s[i];
+		  }
+	  }
+	  return str;
+	  
+  }
 }
 
 // Do not change the code below
