9e57e0c609e5019eeb25591af837766df1b2bfe5
Dhruv Patel <122099280+dhruvurgr8@users.noreply.github.com>
2024-02-26 13:55:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 function firstWord(s) {
-	let str = "";
-  if(s==""){
-	  return s;
-  }else{
-	  for(let i=0;i<s.length;i++){
-		  if(s[i]==" "){
-			  return str;
-		  }else{
-			  str+=s[i];
-		  }
-	  }
-	  return str;
-	  
-  }
-}
+	s = s.trim();
+    let str = "";
+    if (s === "") {
+        return s;
+    } else {
+        for (let i = 0; i < s.length; i++) {
+            if (s[i] === " ") {
+                return str;
+            } else {
+                str += s[i];
+            }
+        }
+        return str;
+    }
+}  
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
-alert(firstWord(s));
+alert(firstWord(s));  
